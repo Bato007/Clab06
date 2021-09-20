4dda7697455daf9f416501541999a23879874bd1
Andrea Amaya <andrea.a@firstlight.ai>
2021-09-19 22:44:11 -0600

    Primer inciso

---
+++ hashlibExamples.py
@@ -0,0 +1,58 @@
+import hashlib
+import codecs
+
+# CIFRADO sha256
+def sha256Res(cadena):
+    m = hashlib.sha256()
+    m.update(cadena)
+
+    resHex = m.hexdigest()
+
+    resBin = bin(int(resHex, 16))[2:].zfill(8)
+    res64 = codecs.encode(codecs.decode(resHex, 'hex'), 'base64').decode()
+
+    return [resBin, resHex, res64, cadena]
+
+
+# CIFRADO sha512
+def sha512Res(cadena):
+    m = hashlib.sha512()
+    m.update(cadena)
+
+    resHex = m.hexdigest()
+
+    resBin = bin(int(resHex, 16))[2:].zfill(8)
+    res64 = codecs.encode(codecs.decode(resHex, 'hex'), 'base64').decode()
+
+    return [resBin, resHex, res64, cadena]
+
+
+# CIFRADO blake2b
+def blake2bRes(cadena, digestSize=64):
+    m = hashlib.blake2b(digest_size=digestSize)
+    m.update(cadena)
+
+    resHex = m.hexdigest()
+
+    resBin = bin(int(resHex, 16))[2:].zfill(8)
+    res64 = codecs.encode(codecs.decode(resHex, 'hex'), 'base64').decode()
+
+    return [resBin, resHex, res64, cadena]   
+
+
+# Mostrar resultado
+def printRes(nombre, res):
+    print('-'*100)
+    print('RESULTADO DE', nombre)
+    print('CADENA PROBADA', res[3])
+    print('-'*100)
+    print('\nBinario', res[0])
+    print('\nHex', res[1])
+    print('\nbase64', res[2])
+    print('-'*100)
+    return ''
+
+
+print(printRes('sha256', sha256Res(b'quiero ir a dormir')))
+print(printRes('sha512', sha512Res(b'quiero ir a dormir')))
+print(printRes('blake2b', blake2bRes(b'quiero ir a dormir', 64)))
